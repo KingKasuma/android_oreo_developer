f540a2ef8ce65394bdd0dfd3daaa310e397eeb2c
Mike <lanselot777hailander@gmail.com>
2018-12-11 21:25:10 -0400

    Sound

--- Animations/app/src/main/java/com/example/mike/animations/MainActivity.java
+++ Animations/app/src/main/java/com/example/mike/animations/MainActivity.java
@@ -8,12 +8,17 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    boolean bartIsShowing = true;
+    //boolean bartIsShowing = true;
     public void fade(View view){
         Log.i("Info", "Imageview tapped");
 
         ImageView bartImageView = (ImageView) findViewById(R.id.bartImageView);
         ImageView homerImageView = (ImageView) findViewById(R.id.homerImageView);
+        //bartImageView.animate().translationXBy(-1000).setDuration(2000);
+        //bartImageView.animate().rotation(1800).alpha(0).setDuration(1000);
+        bartImageView.animate().scaleX(0.5f).scaleY(0.5f).setDuration(1000);
+
+        /*
         if(bartIsShowing){
             bartIsShowing = false;
             bartImageView.animate().alpha(0).setDuration(2000);
@@ -23,11 +28,16 @@ public class MainActivity extends AppCompatActivity {
             bartImageView.animate().alpha(1).setDuration(2000);
             homerImageView.animate().alpha(0).setDuration(2000);
         }
+        */
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView bartImageView = (ImageView) findViewById(R.id.bartImageView);
+        bartImageView.setX(-1000);
+        bartImageView.animate().translationXBy(1000).rotation(3600).setDuration(2000);
     }
 }
---
+++ SoundDemo/app/src/main/java/com/example/mike/sounddemo/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.mike.sounddemo;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    AudioManager audioManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        mediaPlayer = MediaPlayer.create(this, R.raw.marbles);
+
+        SeekBar volumeControl = (SeekBar) findViewById(R.id.volumeSeekBar);
+
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(currentVolume);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("Seekbar changed", Integer.toString(progress));
+
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        final SeekBar scrubSeekbar = (SeekBar) findViewById(R.id.scrubSeekBar);
+        scrubSeekbar.setMax(mediaPlayer.getDuration());
+        scrubSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("Scrub seekbar moved", Integer.toString(progress));
+                mediaPlayer.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                scrubSeekbar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        },0,300);
+    }
+
+    public void play(View view){
+        mediaPlayer.start();
+    }
+
+    public void pause(View view){
+        mediaPlayer.pause();
+    }
+}
