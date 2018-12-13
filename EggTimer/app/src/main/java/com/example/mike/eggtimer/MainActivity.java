0c3f99086e117654e8c0c0003a6c8fc2a3a0d675
Mike <lanselot777hailander@gmail.com>
2018-12-13 17:48:00 -0400

    Timer

---
+++ EggTimer/app/src/main/java/com/example/mike/eggtimer/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.mike.eggtimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekbar = findViewById(R.id.timerSeekBar);
+        TextView timerTextView = findViewById(R.id.countdownTextView);
+
+        timerSeekbar.setMax(600);
+        timerSeekbar.setProgress(30);
+
+        timerSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
